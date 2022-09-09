6d260c15193c2106f18a43b3dc0083aa32c0ec29
Jenn <jenniferjuarezjj@gmail.com>
2022-09-09 15:44:03 -0400

    declared deck variables

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// Declare deck variables
+let deck1 = []
+let deck2 = []
+let deck3 = []
+let deck4 = []
+let cardToRemove
+
+// Cached element references
+
+// Event listeners
+
+// Functions
+
+// Initialize deck 1 with array of 52 cards 
+
+// Function to handle a button click:
+    // Randomly select number from total cards remaining
+    // Assign card with the random index to a variable
+    // Add card picked to deck 2
+    // Pass card picked to render function to display
+
+
+// Function to render deck state
+	 // Remove outline class when first card is picked
+   // Removes previous picked card from deck 2 class list
+   // Add current card picked to deck 2 element
+	 // Adjust shadow when deck gets above/below halfway full
+	 // Remove card back color and add outline when last card is picked
