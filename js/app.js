d88353a00c5724b7e6bc10b1fd227a1d4282b7d9
Jenn <jenniferjuarezjj@gmail.com>
2022-09-09 19:57:46 -0400

    create master deck

--- js/app.js
+++ js/app.js
@@ -5,21 +5,35 @@ let deck3 = []
 let deck4 = []
 let cardToRemove
 
+const masterDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+
 // Cached element references
-let deck1El = document.getElementById('deck-1')
-let deck2El = document.getElementById('deck-2')
-let deck3El = document.getElementById('deck-3')
-let deck4El = document.getElementById('deck-4')
+let deck1El = document.getElementById('deck1')
+let deck2El = document.getElementById('deck2')
+let deck3El = document.getElementById('deck3')
+let deck4El = document.getElementById('deck4')
 
 
 console.log(deck1El, deck2El, deck3El, deck4El)
 // Event listeners
 
-document.getElementById('btn').addEventListener('click', ()=> console.log('clicked'))
+document.getElementById('btn-1').addEventListener('click', ()=> console.log('clicked'))
+document.getElementById('btn-2').addEventListener('click', ()=> console.log('clicked'))
 
 // Functions
 
+init()
+
+
+
 // Initialize deck 1 with array of 52 cards 
+
+function init() {
+  deck2 = [...masterDeck]
+  deck4 = [...masterDeck]
+  console.log(deck2, deck4)
+}
+
 
 // Function to handle a button click:
     // Randomly select number from total cards remaining
