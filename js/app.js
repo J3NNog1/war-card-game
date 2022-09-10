61f50b2e3f1b0879d974decf660ec837b008c768
Jenn <jenniferjuarezjj@gmail.com>
2022-09-10 18:37:01 -0400

    enable class changes

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ let deck1 = []
 let deck2 = []
 let deck3 = []
 let deck4 = []
-let cardToRemove
+let cardToRemove1
+let cardToRemove2
 
 const masterDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
@@ -53,20 +54,27 @@ function handleClick() {
     deck1.push(cardPicked1)
     deck3.push(cardPicked2)
 
-    console.log(deck2, 'deck2')
-    console.log(deck1, 'deck1')
-    console.log(deck3, 'deck3')
-    console.log(deck4, 'deck4')
-  render()
+  render(cardPicked1, cardPicked2)
   }
 }
     
 
 // Function to render deck state
 function render(cardPicked1, cardPicked2) {
-	 // Remove outline class when first card is picked
-   // Removes previous picked card from deck 2 class list
-   // Add current card picked to deck 2 element
-	 // Adjust shadow when deck gets above/below halfway full
-	 // Remove card back color and add outline when last card is picked
+  console.log(cardPicked1, cardPicked2)
+  if (deck1.length === 1) {
+    deck1El.classList.remove('outline')
+    deck3El.classList.remove('outline')
+  }
+  if (deck2.length > 1) {
+    deck2El.classList.remove(cardToRemove1)
+    deck4El.classList.remove(cardToRemove2)
+  }
+  cardToRemove1 = cardPicked1
+  cardToRemove2 = cardPicked2
+  
+  deck2El.classList.add(cardPicked1)
+  deck4El.classList.add(cardPicked2)
+
+	
 }
